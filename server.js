7159b7d02a5ceb7f9c1dcfee55031188efb3eaf6
nguyennhattan1398 <37937759+nguyennhattan1398@users.noreply.github.com>
2021-01-13 16:59:26 +0700

    QA
    A

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const initAPIs = require("./routes/api");
 app.use(express.json());
 initAPIs(app);
-let port = 9090;
+let port = 9000;
 app.listen(port, () => {
   console.log(`running at localhost:${port}/`);
 });
