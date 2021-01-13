2ec86e7d27eb45f279bf4e298cabd217292f66cd
nguyennhattan1398 <37937759+nguyennhattan1398@users.noreply.github.com>
2021-01-13 16:48:55 +0700

    k
    k

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const app = express();
 const initAPIs = require("./routes/api");
 app.use(express.json());
 initAPIs(app);
-let port = 300;
+let port = 9090;
 app.listen(port, () => {
   console.log(`running at localhost:${port}/`);
 });
