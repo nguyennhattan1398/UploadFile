cac772cb4491d2c8f36585bfcbb70b1f5e1f8a1c
nguyennhattan1398 <37937759+nguyennhattan1398@users.noreply.github.com>
2021-01-14 11:31:07 +0700

    1
    1

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose.connect(
-  " mongodb://127.0.0.1:27017/DB_Query",
+  "mongodb+srv://demo:<password>@cluster0.pfpwe.mongodb.net/test",
   { useUnifiedTopology: true, useNewUrlParser: true },
   function (err, db) {
     if (err) throw err;
